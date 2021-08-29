02e88f2ae645b66aa8fefcbcc21750738bc11652
araihara-k <araihara@gmail.com>
2021-08-29 09:09:06 +0900

    Try to create simple recipe applications with react

---
+++ recipes-app/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist', 'assets'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
