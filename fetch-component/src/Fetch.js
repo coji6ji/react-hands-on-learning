5ac253e8969fb0885912125b4395b41f293f605a
araihara-k <araihara@gmail.com>
2021-09-04 08:54:17 +0900

    add a sample about a custom hook called `useFectch` and FetchComponent

---
+++ fetch-component/src/Fetch.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+
+/**
+ * useFetch Hook（fetchリクエストの抽象化）
+ */
+function useFetch(uri) {
+  const [data, setData] = useState()
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (!uri) return
+    fetch(uri)
+      .then(data => data.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError)
+  }, [uri])
+
+  return {
+    loading,
+    data,
+    error,
+  }
+}
+
+/**
+ * Fetch Component（fetchに関連する描画の抽象化）
+ */
+export default function Fetch({
+  uri,
+  renderSuccess,
+  loadingFallback = <p>loading...</p>,
+  renderError = error => <pre>{JSON.stringify(error, null, 2)}</pre>,
+}) {
+  const { loading, data, error } = useFetch(uri)
+  if (error) return renderError(error)
+  if (loading) return loadingFallback
+  if (data) return renderSuccess({ data })
+}
